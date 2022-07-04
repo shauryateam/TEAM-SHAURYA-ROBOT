4db73f3ea052fbc3bd9860875fe9ed74f4567153
SHAURYA_X_TEAM <100112352+shauryateam@users.noreply.github.com>
2022-07-04 09:45:07 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop ShauryaRobot
-net start ShauryaRobot
+net stop FallenRobot
+net start FallenRobot
